5d79c604906806f08c04296fd35884b40265a0b9
AlvesRichard <richard.alves1994@gmail.com>
2021-06-30 18:38:48 -0300

    ruta carrito

--- api/app/routes/shop.js
+++ api/app/routes/shop.js
@@ -13,30 +13,34 @@ router.get("/", (req, res, next) => {
 //AGERGO un nuevo objeto al carrito o lo CREO si ya existe
 router.post("/add", async (req, res, next) => {
     try {
-        const product = req.body.product
-        const userId = req.user.dataValues.id
-        const [carrito, created] = await Carrito.findOrCreate({ where: { userId } })
-        carrito.arrayOfProducts.push(product);
-        await carrito.save()
-        if(created) res.status(201).json(carrito)
-        res.status(200).json(carrito)
-    } catch(err) {
-        next(err)
+        const product = req.body;
+        const userId = req.user.dataValues.id;
+        const [carrito, created] = await Carrito.findOrCreate({
+            where: { userId },
+        });
+        carrito.arrayOfProducts = [...carrito.arrayOfProducts, product];
+        await carrito.save();
+        if (created) res.status(201).json(carrito);
+        res.status(200).json(carrito);
+    } catch (err) {
+        next(err);
     }
-   
 });
 
 //ELIMINO un producto
-router.delete("/:productId",  (req, res, next) => {
-    Carrito.findOne({ where: { userId: req.user.dataValues.id } }).then(carrito => {
-        const productDelete = carrito.arrayOfProducts.findIndex(product => product.id === req.params.productId)
-        carrito.arrayOfProducts.splice(productDelete, 1)
-        return carrito.save()
-    })
-        .then(carritoUpdated => res.status(204).json(carritoUpdated))
-        .catch(next)
-    
-})
+router.delete("/:productId", (req, res, next) => {
+    Carrito.findOne({ where: { userId: req.user.dataValues.id } })
+        .then((carrito) => {
+            const indexDelete = carrito.arrayOfProducts.findIndex(
+                (product) => product.id === req.params.productId
+            );
+            // console.log('INDEX DELETE', indexDelete)
+            carrito.arrayOfProducts.splice(indexDelete, 1);
+            return carrito.save();
+        })
+        .then((carritoUpdated) => res.status(204).json(carritoUpdated))
+        .catch(next);
+});
 
 
 module.exports = router;
