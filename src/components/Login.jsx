ee8e73f50c3ceaf2957be448d7f93d0261163ccc
ajromeroch <ale.romero07@gmail.com>
2021-06-30 13:43:33 -0400

    modificando vista productos

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
-import { setUser } from '../state/user';
-import { useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { setUser } from "../state/user";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import "../styles/Login.css";
 
 function Login() {
@@ -26,28 +26,23 @@ function Login() {
     history.push("/");
   };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        // axios y autenticacion
-        dispatch(setUser(form))
-        history.push('/')
-    }
-
-    return(
-        <div className='login'>
-            <h1>Login</h1>
-            <form className='form' onSubmit={handleSubmit}>
-                <label htmlFor='username'>Username</label>
-                <input onChange={handleChange} type='text' name='username' />
-                <label htmlFor='password'>Password</label>
-                <input onChange={handleChange} type='password' name='username' />
-                <button className='submit' type='submit' value='submit'>Submit</button>
-            </form>
-            <Link to='/register'>
-                <h3>Don't have an account? Register</h3>
-            </Link>
-        </div>
-    )
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input onChange={handleChange} type="text" name="username" />
+        <label htmlFor="password">Password</label>
+        <input onChange={handleChange} type="password" name="username" />
+        <button className="submit" type="submit" value="submit">
+          Submit
+        </button>
+      </form>
+      <Link to="/register">
+        <h3>Don't have an account? Register</h3>
+      </Link>
+    </div>
+  );
 }
 
 export default Login;
