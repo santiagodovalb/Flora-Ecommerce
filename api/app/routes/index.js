40b3af8bc902e59659dab8ef8a845394fe97ff89
caleo1224 <leonardo.campoy1224@gmail.com>
2021-06-30 14:16:01 -0300

    creacion de rutas de user

--- api/app/routes/index.js
+++ api/app/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use('/products', require('./products'))
+router.use('/users', require('./users'))
 
 router.use('/test', (req,res,next) => {
     res.json({nombre: 'Flora'})
---
+++ api/app/routes/users.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const { User } = require("../../db/models");
+
+router.post("/register", (req, res, next) => {
+  User.create({
+    nick: req.body.nick,
+    email: req.body.email,
+    password: req.body.password,
+    direction: req.body.direction,
+    phone: req.body.phone,
+  }).then((user) => res.status(200).send(user));
+});
+
+router.post("/login", // passport.authenticate("local"),//
+ (req, res, next) => {
+  res.send(req.user);
+});
+
+router.post("logout", (req, res, next) => {
+  req.logOut();
+  res.sendStatus(200);
+});
+
+router.put("/:id", (req, res) => {
+  User.update(req.body, { where: { id: req.params.id } }).then(() => {
+    res.sendStatus(201);
+  });
+});
+
+router.get("/me", (req, res, next) => {
+  if (!req.user) return res.sendStatus(401);
+  return res.send(req.user);
+});
+
+
+//rutas para admins
+
+
+router.get("/allusers", async (req, res, next) => {
+  const users = await User.findAll();
+  return res.send(users);
+});
+
+
+router.put("/allUsers/:userId", (req, res, next) => {
+    User.update({ rolId: 1 }, { where: { id: req.params.userId } }).then(() => {
+        res.sendStatus(200);
+    })
+});
+
+router.delete("/allUsers/:userId", (req, res, next) => {
+  const usuarioId = req.params.userId;
+  User.destroy({
+    where: {
+      id: usuarioId,
+    },
+  }).then(() => {
+    res.sendStatus(200);
+  });
+});
+
+
+module.exports = router;
