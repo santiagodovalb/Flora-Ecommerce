9f3edbf6c1bc7dcd2411f5ed95c45bca6fc994c9
AlvesRichard <richard.alves1994@gmail.com>
2021-07-05 16:51:41 -0300

    reviews terminado

--- api/app/routes/index.js
+++ api/app/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.use('/products', require('./products'))
 router.use('/shop', require('./shop'))
 router.use('/users', require('./users'))
+router.use('/reviews', require('./reviews'))
 
 
 router.use('/test', (req,res,next) => {
