d69e0a7f4387496c45a109d3123f90a73dc8a18f
santikundera <santikundera@gmail.com>
2021-07-06 12:01:41 -0300

    checkout terminado

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,8 +3,22 @@ import { useSelector } from 'react-redux'
 import { Link, useHistory } from 'react-router-dom'
 import '../styles/Checkout.css';
 import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setCart } from '../state/cart';
 
 export default function Checkout() {
+
+    const [payments, setPayments] = useState([])
+    const [usedPayment, setUsedPayment] = useState(0)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        axios.get('/api/paymentMethod/')
+        .then(res => res.data)
+        .then(data => setPayments(data))
+    })
 
     const cart = useSelector(state => state.cart)
     const user = useSelector(state => state.user)
@@ -15,8 +29,17 @@ export default function Checkout() {
     }
 
     const handlePago = () => {
-        axios.post('/api/shop/order')
-        history.push('/user')
+        axios.post('/api/shop/order', {PaymentMethodId: usedPayment})
+        .then(res => res.data)
+        .then(order => {
+            console.log(order)
+            dispatch(setCart())
+            history.push('/user')
+        })
+    }
+
+    const handleChange = (e) => {
+        setUsedPayment(e.target.value)
     }
 
     return (
@@ -42,21 +65,22 @@ export default function Checkout() {
             <hr/>
             <div>
             <h1>Metodo de pago</h1>
-            <select name='pago'>
-                <option value='credito'>Tarjeta de credito</option>
-                <option value='debito'>Tarjeta de debito</option>
-                <option value='mercadopago'>Mercadopago</option>
-                <option value='efectivo'>Efectivo</option>
+            <select onChange={handleChange} name='pago' defaultValue={payments[0]?.id}>
+                {payments.map(payment => {
+                    return (
+                        <option value={payment.id}>{payment.type}</option>
+                    )
+                })}
             </select>
             </div>
-            <div>
+            {/* <div>
             <h1>Metodo de envio</h1>
             <select name='envio'>
                 <option value='credito'>Mercado Envios</option>
                 <option value='debito'>Cadeteria</option>
                 <option value='mercadopago'>Retiro en local</option>
             </select>
-            </div>
+            </div> */}
             <div>
             <button onClick={handlePago}type='button'>Confirmar pago</button>
             <button onClick={handleCancel} type='button'>Cancelar</button>
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
+import axios from 'axios'
 import '../styles/User.css'
 
 export default function User() {
+
+    const [orders, setOrders] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/shop/order')
+        .then(res => res.data)
+        .then(ord => setOrders(ord))
+    }, [])
 
     const user = useSelector(state => state.user)
     const history = useHistory()
@@ -14,12 +25,12 @@ export default function User() {
 
     return (
         <div className='userWrapper'>
-            {console.log(user)}
             <h1>{user.nick}</h1>
             <h2>Direccion: {user.direction}</h2>
             <h2>Email: {user.email}</h2>
             <h2>Telefono: {user.phone}</h2>
             <button type='button' onClick={handleClick}>Editar info</button>
+            {console.log('ORDER', orders)}
         </div>
     )
 }
