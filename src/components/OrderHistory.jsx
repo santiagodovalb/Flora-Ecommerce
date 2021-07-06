84d3dc90685801612ccd75d7ac12e0f05ebad409
ajromeroch <ale.romero07@gmail.com>
2021-07-06 17:31:59 -0400

    agregando single Order

--- src/components/OrderHistory.jsx
+++ src/components/OrderHistory.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../styles/OrderHistory.css";
+import { useHistory } from "react-router";
+import { setSingleOrder } from "../state/order";
+import { useDispatch } from "react-redux";
 
 export default function OrderHistory() {
   const [orders, setOrders] = useState([]);
+  const dispatch = useDispatch();
+
+  const history = useHistory();
 
   useEffect(() => {
     axios
@@ -11,6 +17,11 @@ export default function OrderHistory() {
       .then((res) => res.data)
       .then((ord) => setOrders(ord));
   }, []);
+
+  const handleClick = (ord) => {
+    dispatch(setSingleOrder(ord));
+    history.push(`/order/${ord.id}`);
+  };
 
   return (
     <div>
@@ -22,8 +33,9 @@ export default function OrderHistory() {
           <th>Total</th>
         </tr>
         {orders.map((order) => {
+          console.log("order", order);
           return (
-            <tr>
+            <tr onClick={() => handleClick(order)}>
               <td>{order.createdAt.slice(0, 10)}</td>
               <td>{order.id}</td>
               <td>{order.estado}</td>
@@ -35,8 +47,3 @@ export default function OrderHistory() {
     </div>
   );
 }
-
-// fecha
-// id
-// total - plata
-// estado
--- src/state/store.js
+++ src/state/store.js
@@ -1,16 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import logger from 'redux-logger'
-import { productsReducer } from './products'
-import { userReducer } from './user';
-import { cartReducer } from './cart'
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import { productsReducer } from "./products";
+import { userReducer } from "./user";
+import { cartReducer } from "./cart";
+import { singleOrderReducer } from "./order";
 
-const store = configureStore( {
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-    reducer: {
-        products: productsReducer,
-        user: userReducer,
-        cart: cartReducer
-    }
-})
+const store = configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {
+    products: productsReducer,
+    user: userReducer,
+    cart: cartReducer,
+    singleOrder: singleOrderReducer,
+  },
+});
 
-export default store
+export default store;
